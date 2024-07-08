85554a2724b860b6a2a1c55cbfefd80fe501dc3e
haeun228 <123haeun@naver.com>
2024-07-08 13:44:57 +0900

    Add functions to like post and comment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 
-let posts = [];
+let posts = []; // 게시글 저장
+let comments = {}; // 댓글 저장
+let likes = {}; // 게시글 별로 좋아요 누른 사용자 저장
 
 const app = express()
 app.use(express.json())
@@ -10,11 +12,19 @@ app.get('/posts', function (req, res) {
   res.send(posts)
 });
 
-// 특정 글 조회
+// 특정 글 조회 (댓글, 좋아요 포함)
 app.get('/posts/:id', function (req, res) {
   const id = Number(req.params.id);
   const post = posts.find((post) => post.id === id);
-  res.send(post);
+
+  if (post) {
+    const detailedPost = { ...post };
+    detailedPost.comments = comments[id];
+    detailedPost.likes = likes[id];
+    res.send(detailedPost);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id.' });
+  }
 });
 
 // 글 작성 
@@ -23,13 +33,16 @@ app.post('/posts', (req, res) => {
   const ids = posts.map((post) => post.id);
   let postId;
   if (ids.length===0){
-    postId = 0
+    postId = 1
   } else {
     postId = Math.max(...ids) + 1;
   }
   newPost.id = postId;
   newPost.createdAt = new Date();
   newPost.updatedAt = new Date();
+
+  likes[postId] = [];
+  comments[postId] = [];
 
   posts.push(newPost);
   res.status(201).send(newPost);
@@ -56,7 +69,43 @@ app.delete('/posts/:id', (req, res) => {
   const idx = posts.findIndex((post) => post.id === id);
   if (idx>=0) {
     posts.splice(idx, 1);
+    delete likes[id];
+    delete comments[id];
     res.sendStatus(204);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id.' });
+  }
+});
+
+// 게시글 좋아요 & 좋아요 취소
+app.post('/posts/:id/like', (req, res)=> {
+  const postId = Number(req.params.id);
+  const post = posts.find((post) => post.id === postId);
+  const { userId } = req.body;
+  
+  if (post) {
+    const idx = likes[postId].indexOf(userId);
+    if (idx >= 0) {
+      likes[postId].splice(idx, 1);
+      res.send({ message: `User "${userId}" unliked the post.`});
+    } else {
+      likes[postId].push(userId);
+      res.send({ message: `User "${userId}" liked the post.`});
+    }
+  } else {
+    res.status(404).send({ message: 'Cannot find given id.' });
+  }
+});
+
+// 댓글
+app.post('/posts/:id/comment', (req, res) => {
+  const postId = Number(req.params.id);
+  const post = posts.find((post) => post.id === postId);
+  const newComment = req.body;
+
+  if (post) {
+    comments[postId].push(newComment);
+    res.send(comments[postId]);
   } else {
     res.status(404).send({ message: 'Cannot find given id.' });
   }
