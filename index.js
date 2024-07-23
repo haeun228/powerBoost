ef2fd2024c6ba78f4bbaeffd52f5ae8cf96d294f
haeun228 <123haeun@naver.com>
2024-07-23 22:23:59 +0900

    feat: Replace session authentication with JWT token and add error handler

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const session = require('express-session');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
+const {
+  generateAccessToken,
+  authenticateToken,
+} = require('./jwt.js');
 
 const db = require('./models/index');
 const { User, Post, Comment, Like } = db;
 
 const app = express();
 app.use(express.json());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(cookieParser());
+
+class PostNotFoundError extends Error {}
+class UserNotAuthorizedError extends Error {}
+
+function asyncHandler(handler) {
+  return async function (req, res) {
+    try {
+      await handler(req, res);
+    } catch (e) {
+      if (e instanceof PostNotFoundError) {
+        res.status(400).send({ message: 'Cannot find given post id.' });
+      } else if (e instanceof UserNotAuthorizedError) {
+        res.status(401).send({ message: 'You cannot edit/delete this post!' });
+      } else {
+        res.status(500).send({ message: e.message });
+      }
+    }
+  };
+}
 
 // 전체 글 조회
-app.get('/posts', async (req, res) => {
+app.get('/posts', asyncHandler(async (req, res) => {
   const posts = await Post.findAll({
     order: [['createdAt', 'DESC']],
   });
   res.send(posts);
-});
+}));
 
 // 특정 글 조회 (댓글, 좋아요 포함)
-app.get('/posts/:id', async (req, res) => {
+app.get('/posts/:id', asyncHandler(async (req, res) => {
   const postId = req.params.id;
-
-  const post = await Post.findOne({
-    where: { postId },
+  const post = await Post.findByPk(postId, {
     include: [
       {
         model: Comment,
@@ -36,120 +53,97 @@ app.get('/posts/:id', async (req, res) => {
       },
     ],
   });
+  if (!post) throw new PostNotFoundError();
+
   const likes = await Like.count({ where: { postId } });
   post.dataValues.likes = likes;
   res.send(post);
-});
+}));
 
 // 글 작성 
-app.post('/posts', async (req, res) => {
+app.post('/posts', authenticateToken, asyncHandler(async (req, res) => {
   const newPost = req.body;
-  if (!req.session.userId) {
-    return res.status(404).send({ message: 'You have to login first to upload a post' });
-  }
   const post = await Post.create({
-    userId: req.session.userId,
+    userId: req.userId,
     title: newPost.title,
     content: newPost.content
   });
   res.send(post);
-});
+}));
 
 // 글 수정
-app.patch('/posts/:id', async (req, res) => {
+app.patch('/posts/:id', authenticateToken, asyncHandler(async (req, res) => {
   const postId = req.params.id;
   const updatedPost = req.body;
-  const post = await Post.findOne({ where: { postId } })
+  const post = await Post.findByPk(postId);
 
-  if (!post) {
-    return res.status(404).send({ message: 'Cannot find given post id.' });
-  }
-  if (post.userId!==req.session.userId) {
-    return res.status(404).send({ message: 'You cannot update this post' });
-  }
+  if (!post) throw new PostNotFoundError();
+  if (post.userId!==req.userId) throw new UserNotAuthorizedError();
 
   await Post.update(updatedPost, { where: { postId } });
   res.send({ message: 'Post updated successfully!' });
-});
+}));
 
 // 글 삭제
-app.delete('/posts/:id', async (req, res) => {
+app.delete('/posts/:id', authenticateToken, asyncHandler(async (req, res) => {
   const postId = req.params.id;
-  const post = await Post.findOne({ where: { postId } });
-  if (!post) {
-    return res.status(404).send({ message: 'Cannot find given post id.' });
-  }
-  if (post.userId!==req.session.userId) {
-    return res.status(404).send({ message: 'You cannot delete this post' });
-  }
+  const post = await Post.findByPk(postId);
 
-  const deletedCount = await Post.destroy({ where: { postId } });
-  if (deletedCount>0) {
-    res.status(204).send();
-  } else {
-    res.send(404). send({ message:  'Cannot find given post id.' });
-  }
-});
+  if (!post) throw new PostNotFoundError();
+  if (post.userId!==req.userId) throw new UserNotAuthorizedError();
+  
+  await Post.destroy({ where: { postId } });
+  res.status(204).send();
+}));
 
 // 게시글 좋아요 & 좋아요 취소
-app.post('/posts/:id/like', async (req, res)=> {
+app.post('/posts/:id/like', authenticateToken, asyncHandler(async (req, res)=> {
   const postId = req.params.id;
-  const post = await Post.findOne({ where: { postId }});
+  const post = await Post.findByPk(postId);
 
-  if (!req.session.userId) {
-    return res.status(404).send({ message: 'You have to login first to like a post' });
-  }
-
-  if (!post) {
-    return res.status(404).send({ message: 'Cannot find given post id.' });
-  }
+  if (!post) throw new PostNotFoundError();
 
   const isLiked = await Like.findOne({
     where: {
       postId,
-      userId: req.session.userId,
+      userId: req.userId,
     }
   });
   if (!isLiked) {
     await Like.create({
       postId,
-      userId: req.session.userId,
+      userId: req.userId,
     });
     res.send({ message: 'Post liked successfully!'});
   } else {
     await Like.destroy({
       where: {
         postId,
-        userId: req.session.userId,
+        userId: req.userId,
       }
     });
     res.send({ message: 'Post unliked successfully!'});
   }
-});
+}));
 
 // 댓글
-app.post('/posts/:id/comment', async (req, res) => {
+app.post('/posts/:id/comment', authenticateToken, asyncHandler(async (req, res) => {
   const postId = req.params.id;
   const { comment } = req.body;
-  const post = await Post.findOne({ where: { postId }});
+  const post = await Post.findByPk(postId);
 
-  if (!req.session.userId) {
-    return res.status(404).send({ message: 'You have to login first to write comment to a post' });
-  }
-  if (!post) {
-    return res.status(404).send({ message: 'Cannot find given post id.' });
-  }
+  if (!post) throw new PostNotFoundError();
 
   await Comment.create({
     postId,
-    userId: req.session.userId,
+    userId: req.userId,
     content: comment,
   })
   res.send({ message: 'Comment posted successfully' });
-});
+}));
 
 // 회원 가입
-app.post('/signup', async (req, res) => {
+app.post('/signup', asyncHandler(async (req, res) => {
   const { userId, email, password } = req.body;
 
   const isIdDuplicate = await User.findOne({ where: { userId }});
@@ -159,21 +153,21 @@ app.post('/signup', async (req, res) => {
 
   const isEmailDuplicate = await User.findOne({ where: { email }});
   if (isEmailDuplicate) {
-    return res.send({ message: 'Your email already is already used' });
+    return res.send({ message: 'Your email is already used' });
   }
 
   const hashedPassword = await bcrypt.hash(password, saltRounds);
-  const user = await User.create({
+  await User.create({
     userId,
     email,
     password: hashedPassword
   });
 
   res.status(201).send({ message: 'Registration Complete!' });
-});
+}));
 
 // 로그인
-app.post('/login', async (req, res) => {
+app.post('/login', asyncHandler(async (req, res) => {
   const { userId, password } = req.body;
   const user = await User.findOne({ where: { userId }});
 
@@ -183,21 +177,24 @@ app.post('/login', async (req, res) => {
 
   const isPasswordCorrect = await bcrypt.compare(password, user.password);
   if (isPasswordCorrect) {
-    req.session.userId = userId;
+    const accessToken = generateAccessToken(user.userId);
+    
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      // secure: true, // 로컬호스트에서 실행해보려면 주석 처리
+      maxAge: 10 * 60 * 1000 // 10분
+    });
+
     res.send({ message: 'Login Succeeded!' });
   } else {
     res.status(401).send({ message: 'Invalid password!' });
   }
-});
+}));
 
 // 로그아웃
-app.get('/logout', (req, res) => {
-  req.session.destroy((error) => {
-    if (error) {
-      return res.send({ message: 'Error logging out' });
-    }
-  });
+app.get('/logout', asyncHandler((req, res) => {
+  res.clearCookie('accessToken');
   res.send({ message: 'Logout Succeeded!' });
-});
+}));
 
 app.listen(3000, () => console.log('Server Started'));
---
+++ jwt.js
@@ -0,0 +1,87 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const db = require('./models/index');
+const { User } = db;
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+const generateAccessToken = (userId) => {
+  return jwt.sign(
+    { id: userId },
+    SECRET_KEY,
+    { expiresIn: "10m" }
+  );
+};
+
+// const generateRefreshToken = () => {
+//   return jwt.sign(
+//     {},
+//     SECRET_KEY,
+//     { expiresIn: "1d" }
+//   );
+// };
+
+const verifyAccessToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    return {
+      ok: true,
+      id: decoded.id,
+    };
+  } catch (err) {
+    return {
+      ok: false,
+      message: err.message
+    };
+  }
+};
+
+// const verifyRefreshToken = async (token) => {
+//   try {
+//     const decoded = jwt.verify(token, SECRET_KEY);
+//     const user = await User.findOne({ where: { userId: decoded.id } });
+//     if (user) {
+//       return {
+//         ok: true,
+//         id: decoded.id
+//       };
+//     } else {
+//       return {
+//         ok: false,
+//         message: "Invalid user",
+//       };
+//     }
+//   } catch (err) {
+//     return {
+//       ok: false,
+//       message: err.message,
+//     };
+//   }
+// };
+
+const authenticateToken = (req, res, next) => {
+  const token = req.cookies.accessToken;
+
+  if (token == null) return res.status(401).send({ message: 'You have to login first!' });
+
+  const result = verifyAccessToken(token);
+  if (result.ok) {
+    req.userId = result.id;
+
+    const newToken = generateAccessToken(result.id);
+    res.cookie('accessToken', newToken, {
+      httpOnly: true,
+      // secure: true,
+      maxAge: 10*60*1000
+    });
+    next();
+  } else {
+    res.status(403).send({ message: 'Invalid or expired token' });
+  }
+};
+
+module.exports = {
+  generateAccessToken,
+  verifyAccessToken,
+  authenticateToken
+};
